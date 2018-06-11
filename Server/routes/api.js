a6dee773716cbcbc6ab1d7c2f161173688ed9069
Skelyos <pat123john123@gmail.com>
2018-06-11 20:39:29 +0100

    Comit web.config & angular user db for main db

--- Server/routes/api.js
+++ Server/routes/api.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://Yuno:Password@localhost:27017';
+const url = "mongodb://Angular:jz0pXKkBrcv0668M@yuno-shard-00-00-vxsxf.mongodb.net:27017,yuno-shard-00-01-vxsxf.mongodb.net:27017,yuno-shard-00-02-vxsxf.mongodb.net:27017/test?ssl=true&replicaSet=Yuno-shard-0&authSource=admin";
+// const url = 'mongodb://Yuno:Password@localhost:27017';
 
 router.get('/', (req, res) => {
     res.send('From API route');
